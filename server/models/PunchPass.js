68aec36d4a48fdb6c64c3ebcd9509152c1e169bc
Jaxon Adams <jadamsresume1@gmail.com>
2022-06-27 21:48:43 -0600

    quick modification to model

--- server/models/PunchPass.js
+++ server/models/PunchPass.js
@@ -16,21 +16,32 @@ const punchPassSchema = new Schema(
         punches: {
             type: Number,
             required: true,
-            default: 0,
-            set: function(v) {
-                if (v >= 8) {
-                    console.log(`${this.name}'s pass is almost full!`);
-                }
-            }
+            default: 0
         }
     },
-    {}
+    {
+        toJSON: {
+            virtuals: true
+        }
+    }
 );
 
 punchPassSchema.virtual('overduePunches').get(function() {
     const overduePunches = this.punches - 10;
     if (overduePunches > 0) {
         return overduePunches;
+    }
+});
+
+// Set a virtual value based on if the punch pass is full, nearly full, or not nearly full.
+// 2 for full, 1 for nearly full, 0 for not full or nearly full
+punchPassSchema.virtual('isNearlyFull').get(function() {
+    if (this.punches >= 10) {
+        return 2;
+    } else if (this.punches >= 8) {
+        return 1;
+    } else {
+        return 0;
     }
 });
 
