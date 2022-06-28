f59e901aa9962bbdc4904a9616c6dfef6569b45c
Jaxon Adams <jadamsresume1@gmail.com>
2022-06-27 19:16:59 -0600

    boilerplate express server

--- server/server.js
+++ server/server.js
@@ -1,1 +1,10 @@
-// express server setup here
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
