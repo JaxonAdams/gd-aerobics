bca599080ce0700d07c5c96eb609c69568f96cd8
Jaxon Adams <jadamsresume1@gmail.com>
2022-07-01 15:09:24 -0600

    set up server-side jwt logic and login route

--- server/controllers/user-controllers.js
+++ server/controllers/user-controllers.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const { User } = require('../models');
+const { signToken } = require('../utils/auth');
 
 // GET all users /api/users
 router.get('/', (req, res) => {
@@ -14,12 +15,28 @@ router.get('/', (req, res) => {
 // POST add user /api/users
 router.post('/', async ({ body }, res) => {
     // expects { email: example@gmail.com, password: somepassword }
-    User.create(body)
-    .then(dbUserData => res.json(dbUserData))
-    .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-    });
+    const user = await User.create(body).select('-__v -password');
+    const token = signToken(user);
+
+    res.json({ token, user });
+});
+
+// POST login user /api/users/login
+router.post('/login', async ({ body }, res) => {
+    const user = await User.findOne({ email: body.email }).select('-__v');
+
+    if (!user) {
+        return res.status(500).json({ message: 'Invalid email.' });
+    }
+
+    const correctPassword = await user.isCorrectPassword(body.password);
+
+    if (!correctPassword) {
+        return res.status(500).json({ message: 'Invalid password.' });
+    }
+
+    const token = signToken(user);
+    res.json({ token, user });
 });
 
 module.exports = router;
