6f788282d28852bb40c38cf98ec46782fd182afc
Jaxon Adams <jadamsresume1@gmail.com>
2022-12-05 20:37:29 -0700

    updated footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Footer = ({ passList }) => {
+    const [passCount, setPassCount] = useState({});
+
+    useEffect(() => {
+        axios.get('/api/passes/info/count')
+        .then(dbPassData => setPassCount(dbPassData.data));
+    }, []);
+
     return (
         <footer className='footer'>
             <h2 className='footer-txt'>Gotta Dance 2022</h2>
-            <p className='footer-txt pass-count'>{passList.length} Passes Logged</p>
+            <p className='footer-txt pass-count'>Logged: {passCount.regular} Regular ~ {passCount.unlimited} Unlimited</p>
         </footer>
     );
 };
